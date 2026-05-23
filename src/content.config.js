38617a0ced75a4ffecb8910f5ed706d0e2e87f43
leslylulu <dearlulu1995@gmail.com>
2026-05-23 15:39:19 -0400

    feat: generate pages from collection

---
+++ src/content.config.js
@@ -0,0 +1,23 @@
+import { glob } from "astro/loaders";
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+
+const blog = defineCollection({
+	loader: glob({ pattern: "**/[^_]*.md", base: "./src/pages"}),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.date(),
+		description: z.string(),
+		author: z.string(),
+		image: z.object({
+			url: z.string(),
+			alt: z.string(),
+		}),
+		tags: z.array(z.string()),
+	})
+});
+
+export const collections = { 
+	blog,
+}
--- src/pages/posts/post-1.md
+++ src/pages/posts/post-1.md
@@ -1,5 +1,4 @@
 ---
-layout: ../../layouts/MarkdownPostLayout.astro
 title: 'My First Blog Post'
 pubDate: 2025-05-22
 description: 'This is the first post of my new Astro blog.'
